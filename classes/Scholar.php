fd512f38e1fed7e865f3463b2089e03a5f78d687
FaabsD <fabian.et.je@hotmail.com>
2022-04-08 11:26:56 +0200

    changed function names, started to prepare the make_api_call function to make real API calls

--- classes/Scholar.php
+++ classes/Scholar.php
@@ -1,10 +1,27 @@
 <?php
+
+/**
+ * Scholar
+ * We will be using this class to make API calls to the SerpApi
+ * and to return the data we need.
+ */
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+
 class Scholar
 {
     private $url;
 
     private $jsonData;
     private $return = array();
+
+    private array $defaultArgs = [
+        "engine"    => "google_scholar_author",
+        "hl"        => "",
+        "author_id" => "",
+        "num"       => "",
+        "start"     => "",
+    ];
 
     /**
      * @param $api_link
@@ -15,71 +32,50 @@ class Scholar
     }
 
     /**
-     * @param $url
-     * @return void
+     * @param array $args
+     * @return false|string
      */
-    private function getPubs( $url )
+    public function make_api_call( $key = null, $args = [] )
     {
-        $this->jsonData = file_get_contents($url);
-    }
-
-    /**
-     * @param $json
-     * @return mixed
-     */
-    private function parse_json_data( $json )
-    {
-        return json_decode($json, true);
-    }
-
-    private function get_remote_file_mime( $url )
-    {
-        $response = wp_remote_get($url);
-
-        return wp_remote_retrieve_header($response, 'content-type');
-    }
-
-    /**
-     * @return array
-     */
-    public function read_data()
-    {
-        $this->getPubs($this->url);
-        $decodedJson = $this->parse_json_data($this->jsonData);
-//        error_log('========[Google Scholar Data]========');
-
-        foreach ( $decodedJson['organic_results'] as $index => $result ) {
-//            error_log('title =>' . $result['title']);
-            $myArr = array();
-            $myArr['title'] = $result['title'];
-
-            if ( array_key_exists('resources', $result) ) {
-                $sourceUrl = $result['resources'][0]['link'];
-
-                $myArr['url'] = $sourceUrl;
-//                error_log('resource-link => ' . $sourceUrl);
-                if (isset($sourceUrl) && !empty($sourceUrl)) {
-
-                    $fileType = $this->get_remote_file_mime($sourceUrl);
-
-                    if ( isset($fileType) ) {
-//                        error_log('resource-file-type => ' . $fileType);
-
-                        if ( str_contains($fileType, 'application/pdf') ) {
-//                        error_log('This publication source is a PDF');
-                        } elseif ( str_contains($fileType, 'text/html') ) {
-                            $myArr['sourceType']  = 'html';
-
-                        }
-                    }
+        $this->jsonData = file_get_contents($this->url);
+        if ( isset($key) ) {
+            if ( count($args) >= 1 ) {
+                $args = array_merge($this->defaultArgs, $args);
+                if (defined('WP_DEBUG')) {
+                    error_log('======= API arguments ====');
+                    error_log(print_r($args, true));
                 }
-
             }
-
-            $this->return[$index] = $myArr;
         }
 
-        return $this->return;
+        return $this->jsonData;
+    }
+
+    /**
+     * Return only the relevant Article data (title, link, authors, year)
+     * and pagination data
+     * @param $json
+     * @return false|string
+     */
+    public function strip_data( $json )
+    {
+        $decodedJson = json_decode($json);
+//        error_log('========[Google Scholar Data]========');
+//        error_log('==== data ====');
+        foreach ( $decodedJson->articles as $index => $article ) {
+            $newArr = array();
+
+            $newArr['title'] = $article->title;
+            $newArr['link'] = $article->link;
+            $newArr['authors'] = explode(', ', $article->authors);
+            $newArr['year'] = $article->year;
+
+            $this->return[$index] = $newArr;
+
+//            error_log(print_r($newArr, true));
+        }
+
+        return json_encode($this->return);
 
     }
 }
