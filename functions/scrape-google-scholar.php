9e1b1f6338e2f91554525ad60ced5b0175dc8ae6
FaabsD <fabian.et.je@hotmail.com>
2022-04-08 11:30:25 +0200

    make run function log the stripped response from the (fake) api call

--- functions/scrape-google-scholar.php
+++ functions/scrape-google-scholar.php
@@ -1,35 +1,48 @@
 <?php
 
-//add_action('init', 'run');
+
+add_action('init', 'run');
 function run()
 {
-
+    $count = 0;
+    $args = array(
+        'author_id' => 'cRt7JiIAAAAJ',
+        'hl'        => 'nl'
+    );
     $scholar = new Scholar(THEME_URL . '/resources/dummy.json');
 
-    $data = $scholar->read_data();
+    $originalData = $scholar->make_api_call(SERPAPI_API_KEY, $args);
+    $newData = $scholar->strip_data($originalData);
 
-    foreach ( $data as $item ) {
-//        error_log(print_r($item, true));
 
-        if ( array_key_exists('url', $item) ) {
+    $decodedData = json_decode($newData);
 
-            $html = returnHtmlBody($item['url']);
-            error_log($html);
+    foreach ( $decodedData as $article ) {
+        $count++;
+
+        if ( $count <= 50 ) {
+
+            if ( defined('WP_DEBUG') ) {
+                error_log('========== ARTICLE DATA =========');
+                error_log('TITLE: ' . $article->title);
+                error_log('AUTHORS: ' . print_r($article->authors, true));
+                error_log('URL: ' . $article->link);
+                error_log('PUBLICATION YEAR: ' . $article->year);
+//                sleep(5);
+            }
         }
     }
 }
 
 /**
- * @param $html
- * @return mixed
+ * @param $url
+ * @return bool|DOMDocument
  */
-function returnHtmlBody( $html )
+function openScholarUrl( $url )
 {
-    $dom = new DOMDocument();
-    @$dom->loadHTMLFile($html);
+    $xml = new DOMDocument();
+    $dom = $xml->loadHTMLFile($url, LIBXML_NOERROR);
 
-    $article = $dom->getElementsByTagName('article');
-
-    return count($article);
-
+    return $dom;
 }
+
