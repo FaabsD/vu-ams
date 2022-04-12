74169c6c5ee6ea0f7ad6eb886d3bb27cf5279530
FaabsD <fabian.et.je@hotmail.com>
2022-04-12 15:42:16 +0200

    linked update-publication functions to functions.php

--- functions.php
+++ functions.php
@@ -6,3 +6,5 @@ require_once 'functions/init.php';
 require_once 'functions/posttypes.php';
 require_once 'functions/enqueue.php';
 require_once 'functions/scrape-google-scholar.php';
+
+require_once 'functions/update-publication.php';
