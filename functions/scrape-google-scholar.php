3c0f7a4cef27f0f52794bbaa3e4bafe573f01384
FaabsD <fabian.et.je@hotmail.com>
2022-04-15 15:50:36 +0200

    modified the run function to make real api calls with consecutive calls if the response data contains serpapi_pagination with a next link. Also renamed run function to retrieve_gooogle_scholar_publications

--- functions/scrape-google-scholar.php
+++ functions/scrape-google-scholar.php
@@ -5,39 +5,32 @@ if ( !function_exists( 'post_exists' ) ) {
 
 
 // add_action( 'init', 'run' );
-add_action('retrieve_scholar_publications', 'run');
-function run()
+add_action( 'retrieve_scholar_publications', 'retrieve_google_scholar_publications' );
+function retrieve_google_scholar_publications()
 {
+    // we'll set a variable to determine if the while loop
+    // for consecutive calls should continue or stop.
     $runContinues = true;
+
+    // set the arguments for the api call in a variable.
     $args = array(
         'engine'    => 'google_scholar_author',
         'hl'        => 'nl',
         'author_id' => 'cRt7JiIAAAAJ',
-        'num'       => 0,
+        'num'       => 100,
         'start'     => 0,
     );
     $scholar = new Scholar( THEME_URL . '/resources/dummy.json' );
 
     $originalData = $scholar->make_api_call( SERPAPI_API_KEY, $args );
 
-    error_log('========== DATA FROM API CALL ==========');
-    error_log(print_r($originalData, true));
-    return false;
-
-    $newData = $scholar->strip_data( $originalData );
-
-    $decodedResponseData = json_decode( $originalData );
-
-    $decodedData = json_decode( $newData );
-
-    foreach ( $decodedData as $article ) {
+    foreach ( $originalData->articles as $article ) {
         if ( defined( 'WP_DEBUG' ) ) {
             error_log( '========== ARTICLE DATA =========' );
             error_log( 'TITLE: ' . $article->title );
             error_log( 'AUTHORS: ' . print_r( $article->authors, true ) );
             error_log( 'URL: ' . $article->link );
             error_log( 'PUBLICATION YEAR: ' . $article->year );
-//                sleep(5);
         }
 
         $postArgs = array(
@@ -45,32 +38,40 @@ function run()
         );
 
         if ( post_exists( $article->title ) === 0 ) {
+            // TODO: save more (wished) data to a publication
             save_publication( $article->title, $postArgs, $article->link );
         }
     }
 
-    $calCount = 1;
+    $callCount = 1;
 
-    // TODO: Make a new call to next page if pagination present
-    if ( isset( $decodedResponseData->serpapi_pagination ) && isset( $decodedResponseData->serpapi_pagination->next ) ) {
+    // Make following calls if a serpapi_pagination is present in
+    // the first call response & if it contains a next link
+    if ( isset( $originalData->serpapi_pagination ) && isset( $originalData->serpapi_pagination->next ) ) {
         // fire a new API call
         while ( $runContinues ) {
-            $data = $scholar->make_api_call( SERPAPI_API_KEY, array( 'start' => $calCount * 100 ) );
-            $calCount++;
-            $strippedData = $scholar->strip_data( $data );
+            $data = $scholar->make_api_call( SERPAPI_API_KEY, array( 'num' => 100, 'start' => $callCount * 100 ) );
+            $callCount++;
 
-            $articleData = json_decode( $strippedData );
+            if ( defined( 'WP_DEBUG' ) ) {
+                error_log( '========== API CALL ' . $callCount . 'DATA ==========' );
+                error_log( print_r( $data, true ) );
+            }
 
-            foreach ( $articleData as $article ) {
+            // loop through the articles from the new call
+            foreach ( $data->articles as $article ) {
                 $postArgs = array(
                     'post_content' => implode( ', ', $article->authors ),
                 );
-
-                if (post_exists($article->title) === 0 ) {
-                    save_publication($article->title, $postArgs, $article->link);
+                // save any post that doesn't exist by the given title
+                if ( post_exists( $article->title ) === 0 ) {
+                    // TODO: save more (wished) data to a publication
+                    save_publication( $article->title, $postArgs, $article->link );
                 }
             }
-            if (!isset($data->serpapi_pagination)) {
+            // set $runContinues to false. To stop the while loop
+            // if there is no pagination present in the response
+            if ( !isset( $data->serpapi_pagination ) ) {
                 $runContinues = false;
             }
         }
@@ -88,6 +89,7 @@ function run()
  */
 function save_publication( $title, $args, $url )
 {
+    // TODO: save more (wished) data to a publication
     $defaults = array(
         'post_title'     => $title,
         'post_content'   => '',
