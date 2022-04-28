c338aa7e5d38b34602657508632e57f9772eb3f7
FaabsD <fabian.et.je@hotmail.com>
2022-04-28 13:09:57 +0200

    basically finished styling the homepage header

--- page-home.php
+++ page-home.php
@@ -6,39 +6,48 @@
 
 <!-- Homepage Header -->
 <header class="header">
-    <div class="header__text">
-        <?php if ( get_field( 'header_text' ) && !empty( get_field( 'header_text' ) ) ) : ?>
-            <?php the_field( 'header_text' ); ?>
-        <?php endif; ?>
+    <div class="header__content">
+        <div class="header__text">
+            <?php if ( get_field( 'header_text' ) && !empty( get_field( 'header_text' ) ) ) : ?>
+                <?php the_field( 'header_text' ); ?>
+            <?php endif; ?>
+        </div>
+        <div class="header__buttons">
+            <?php if ( have_rows( 'homepage_header_buttons' ) ) : while ( have_rows( 'homepage_header_buttons' ) ) : the_row(); ?>
+                <?php
+                $btn1 = get_sub_field( 'header_button_1' );
+                $btn2 = get_sub_field( 'header_button_2' );
+                ?>
+
+                <a href="<?php echo $btn1['url'] ?>" class="buttons__btn1" target="<?php echo $btn1['target'] ?>"
+                   title="<?php echo $btn1['title'] ?>">
+                    <?php echo $btn1['title'] ?>
+                </a>
+
+                <a href="<?php echo $btn2['url'] ?>" class="buttons__btn2" target="<?php echo $btn2['target'] ?>"
+                   title="<?php echo $btn2['title'] ?>">
+                    <?php echo $btn2['title'] ?>
+                </a>
+            <?php endwhile; endif; ?>
+        </div>
     </div>
-    <div class="header__buttons">
-        <?php if ( have_rows( 'homepage_header_buttons' ) ) : while ( have_rows( 'homepage_header_buttons' ) ) : the_row(); ?>
-            <?php
-            $btn1 = get_sub_field( 'header_button_1' );
-            $btn2 = get_sub_field( 'header_button_2' );
-            ?>
+    <div class="header__shape"></div>
 
-            <a href="<?php echo $btn1['url'] ?>" class="buttons__btn1" target="<?php echo $btn1['target'] ?>"
-               title="<?php echo $btn1['title'] ?>">
-                <?php echo $btn1['title'] ?>
-            </a>
 
-            <a href="<?php echo $btn2['url'] ?>" class="buttons__btn2" target="<?php echo $btn2['target'] ?>"
-               title="<?php echo $btn2['title'] ?>">
-                <?php echo $btn2['title'] ?>
-            </a>
-        <?php endwhile; endif; ?>
-    </div>
-    <svg viewBox="0 0 295 326" xmlns="http://www.w3.org/2000/svg" class="header__shape">
-        <defs>
-            <linearGradient id="myGradient">
-                <stop offset="5%" stop-color="skyblue"/>
-                <stop offset="95%" stop-color="#424242"/>
-            </linearGradient>
-        </defs>
+    <!-- Header images -->
+    <?php if ( get_field( 'header_image_1' ) || get_field( 'header_image_2' ) ) : ?>
+        <div class="header__images">
+            <div class="header__images--1">
+                <?php $img1 = get_field( 'header_image_1' ); ?>
+                <img src="<?php echo $img1['url'] ?>" alt="<?php echo $img1['alt'] ?>">
+            </div>
+            <div class="header__images--2">
+                <?php $img2 = get_field( 'header_image_2' ); ?>
+                <img src="<?php echo $img2['url'] ?>" alt="<?php echo $img2['alt'] ?>">
+            </div>
 
-        <path d="M1 324.5V1H294.5V299L1 324.5Z"/>
-    </svg>
+        </div>
+    <?php endif; ?>
 
 </header>
 
