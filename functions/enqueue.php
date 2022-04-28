fd2da200c32595ff22b54990e1716e3a3b86c257
FaabsD <fabian.et.je@hotmail.com>
2022-04-28 15:45:56 +0200

    added jquery as a dependency to the enqueued script

--- functions/enqueue.php
+++ functions/enqueue.php
@@ -10,7 +10,7 @@ add_action('wp_enqueue_scripts', 'enqueue_scripts');
 
 function enqueue_scripts()
 {
-    wp_enqueue_script('app', THEME_URL . '/build/scripts/app.js', array(), theme_version(), true);
+    wp_enqueue_script('app', THEME_URL . '/build/scripts/app.js', array('jquery'), theme_version(), true);
 
     //localise THEME_URL to use in script
     $localize = array('theme_url' => THEME_URL);
