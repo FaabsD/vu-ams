dee9c826dec6cf0d760a0980be78e1d80d9b3040
FaabsD <fabian.et.je@hotmail.com>
2022-05-03 15:09:11 +0200

    fixed not updating controls, started working on better lightning

--- resources/assets/scripts/app.js
+++ resources/assets/scripts/app.js
@@ -18,25 +18,26 @@ if (threeContainer) {
     const renderer = new THREE.WebGLRenderer({alpha: true, antialias: true});
 
     // set camera position
-    camera.position.set(0, 0, 25);
+    camera.position.set(0, 0, -90);
 
     renderer.setSize(containerWidth, containerHeight);
-    renderer.setClearColor('#6a6b6a', 1);
+    renderer.setClearColor('#6a6b6a', .5);
 
-    const directionalLight = new THREE.DirectionalLight('#fff', 1.0);
-
-    directionalLight.position.set(10, 10, 15);
-
-    scene.add(directionalLight);
+    const controls = new ArcballControls(camera, renderer.domElement, scene);
+    controls.enableZoom  = false;
+    controls.update();
+    controls.setGizmosVisible(true);
 
     const loader = new OBJLoader();
 
     loader.load(
         directory_uri.theme_url + '/resources/assets/models/dondedag ams.obj',
         function (object) {
-            object.position.set(0, -10, -75);
+            object.position.set(0, -10, 0);
             scene.add(object);
+            setupSpotLight();
             renderer.render(scene, camera);
+            console.log(camera);
         },
         function (xhr) {
             console.log((xhr.loaded / xhr.total * 100) + '% loaded');
@@ -49,21 +50,42 @@ if (threeContainer) {
 
     threeContainer.appendChild(renderer.domElement);
 
-    const controls = new ArcballControls(camera, renderer.domElement, scene);
-    controls.setGizmosVisible(true);
-
     window.addEventListener('resize', onWindowResize);
+
+    function setupSpotLight() {
+        const spotlight = new THREE.SpotLight(0xffffff, 1.6);
+        spotlight.position.set(-5, 5, 15);
+        spotlight.castShadow = true;
+
+        spotlight.shadow.mapSize.width = threeContainer.clientWidth;
+        spotlight.shadow.mapSize.height = threeContainer.clientHeight;
+
+        spotlight.shadow.camera.near = 0.1;
+        spotlight.shadow.camera.far = 100;
+        spotlight.shadow.camera.fov = 50;
+
+        // scene.add(spotlight);
+        camera.add(spotlight);
+
+        if (debugMode) {
+            const SpotlightHelper = new THREE.SpotLightHelper(spotlight, 'rgb(255, 255, 0)');
+            scene.add(SpotlightHelper);
+        }
+    }
 
     function onWindowResize() {
         camera.aspect = threeContainer.clientWidth / threeContainer.clientHeight;
         camera.updateProjectionMatrix();
         renderer.setSize(threeContainer.clientWidth, threeContainer.clientHeight);
+        renderer.render(scene, camera);
     }
 
     function animate() {
         requestAnimationFrame(animate);
         renderer.render(scene, camera);
     }
+
+    animate();
 
 
 }
