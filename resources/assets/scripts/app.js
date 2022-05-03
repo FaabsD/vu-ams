64d07d22a4b14486ba91d096edcb78a7b19d81e1
FaabsD <fabian.et.je@hotmail.com>
2022-05-03 13:16:32 +0200

    fixed threeJS loaded 3d object position

--- resources/assets/scripts/app.js
+++ resources/assets/scripts/app.js
@@ -1,5 +1,6 @@
 // For debugging set const debugMode to true
 const {OBJLoader} = require("three/examples/jsm/loaders/OBJLoader");
+const {ArcballControls} = require("three/examples/jsm/controls/ArcballControls");
 const debugMode = true;
 
 // Loading a 3d render
@@ -14,12 +15,17 @@ if (threeContainer) {
 
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(50, containerWidth / containerHeight, 0.1, 100);
-    const renderer = new THREE.WebGLRenderer();
+    const renderer = new THREE.WebGLRenderer({alpha: true, antialias: true});
+
+    // set camera position
+    camera.position.set(0, 0, 25);
 
     renderer.setSize(containerWidth, containerHeight);
-    renderer.setClearColor('#f1f1f1', 0);
+    renderer.setClearColor('#6a6b6a', 1);
 
     const directionalLight = new THREE.DirectionalLight('#fff', 1.0);
+
+    directionalLight.position.set(10, 10, 15);
 
     scene.add(directionalLight);
 
@@ -28,6 +34,7 @@ if (threeContainer) {
     loader.load(
         directory_uri.theme_url + '/resources/assets/models/dondedag ams.obj',
         function (object) {
+            object.position.set(0, -10, -75);
             scene.add(object);
             renderer.render(scene, camera);
         },
@@ -41,6 +48,22 @@ if (threeContainer) {
     );
 
     threeContainer.appendChild(renderer.domElement);
+
+    const controls = new ArcballControls(camera, renderer.domElement, scene);
+    controls.setGizmosVisible(true);
+
+    window.addEventListener('resize', onWindowResize);
+
+    function onWindowResize() {
+        camera.aspect = threeContainer.clientWidth / threeContainer.clientHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(threeContainer.clientWidth, threeContainer.clientHeight);
+    }
+
+    function animate() {
+        requestAnimationFrame(animate);
+        renderer.render(scene, camera);
+    }
 
 
 }
