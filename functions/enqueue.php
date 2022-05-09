a5ec0ceb61c4030ce6b0a2ac118a7d704bf421ba
FaabsD <fabian.et.je@hotmail.com>
2022-05-09 19:11:30 +0200

    enqueued slick script and minimal css

--- functions/enqueue.php
+++ functions/enqueue.php
@@ -4,6 +4,7 @@ add_action('wp_enqueue_scripts', 'enqueue_styles');
 function enqueue_styles()
 {
     wp_enqueue_style('VU-AMS_style', THEME_URL . '/build/css/app.css');
+    wp_enqueue_style('slickcss', THEME_URL. '/resources/slick/slick.css');
 }
 
 add_action('wp_enqueue_scripts', 'enqueue_scripts');
@@ -11,6 +12,7 @@ add_action('wp_enqueue_scripts', 'enqueue_scripts');
 function enqueue_scripts()
 {
     wp_enqueue_script('app', THEME_URL . '/build/scripts/app.js', array('jquery'), theme_version(), true);
+    wp_enqueue_script('slickslider', THEME_URL . '/resources/slick/slick.js', array('jquery'), theme_version(), true);
 
     //localise THEME_URL to use in script
     $localize = array('theme_url' => THEME_URL);
