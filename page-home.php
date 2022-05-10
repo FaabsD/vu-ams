573a4666599f6db2602aa171a9d1d64656e5b137
FaabsD <fabian.et.je@hotmail.com>
2022-05-10 14:10:35 +0200

    finished slider with styling and fixed a little styling error on the call to action section

--- page-home.php
+++ page-home.php
@@ -233,55 +233,8 @@
     <?php endwhile; endif; ?>
 </div>
 
-<!-- Call to action -->
-<div class="call-to-action">
-    <section>
-        <?php if ( get_field( 'call_to_action_head' ) ): ?>
-            <?php $call_to_action_head = colorize_last_string_word( get_field( 'call_to_action_head' ) ) ?>
-            <h3 class="call-to-action__heading">
-                <?php echo $call_to_action_head; ?>
-            </h3>
-        <?php endif; ?>
 
-        <?php if ( get_field( 'call_to_action_text' ) ) : ?>
-            <?php the_field( 'call_to_action_text' ); ?>
-        <?php endif; ?>
-    </section>
-    <?php if ( get_field( 'call_to_action_form' ) ) : ?>
-        <section>
-            <?php the_field( 'call_to_action_form' ); ?>
-        </section>
-    <?php endif; ?>
-</div>
-
-<!-- Meat the Team -->
-<div class="meet-the-team">
-    <?php
-    $memberArgs = array( 'post_type' => 'team-member' );
-    $teamMembers = new WP_Query( $memberArgs );
-    ?>
-
-    <?php if ( $teamMembers->have_posts() ) : ?>
-        <div class="meet-the-team__members">
-            <?php while ( $teamMembers->have_posts() ) : $teamMembers->the_post(); ?>
-                <div class="members__member">
-                    <div class="member__image-block">
-                        <?php the_post_thumbnail(); ?>
-                    </div>
-                    <div class="member__info-block">
-                        <h2 class="member__heading">
-                            <?php the_title(); ?>
-                        </h2>
-                        <p class="member__text">
-                            <?php the_excerpt(); ?>
-                        </p>
-                    </div>
-                </div>
-            <?php endwhile; ?>
-        </div>
-    <?php endif; ?>
-</div>
-
+<!-- What is -->
 <div class="what-is">
     <?php if ( get_field( 'what_is_title' ) ): ?>
 
@@ -308,6 +261,85 @@
     <div id="THREE_container">
 
     </div>
+</div>
+
+<!-- Call to action -->
+<div class="call-to-action">
+    <section>
+        <?php if ( get_field( 'call_to_action_head' ) ): ?>
+            <?php $call_to_action_head = colorize_last_string_word( get_field( 'call_to_action_head' ) ) ?>
+            <h3 class="call-to-action__heading">
+                <?php echo $call_to_action_head; ?>
+            </h3>
+        <?php endif; ?>
+
+        <?php if ( get_field( 'call_to_action_text' ) ) : ?>
+            <?php the_field( 'call_to_action_text' ); ?>
+        <?php endif; ?>
+    </section>
+    <?php if ( get_field( 'call_to_action_form' ) ) : ?>
+        <section>
+            <?php the_field( 'call_to_action_form' ); ?>
+        </section>
+    <?php endif; ?>
+</div>
+
+<!-- Meet the Team -->
+<div class="meet-the-team">
+    <?php
+    $memberArgs = array( 'post_type' => 'team-member' );
+    $teamMembers = new WP_Query( $memberArgs );
+    ?>
+
+    <?php if ( get_field( 'meet_the_team_head' ) ) : ?>
+        <?php
+        $meet_heading = colorize_last_string_word( get_field( 'meet_the_team_head' ) );
+        ?>
+        <div class="meet-the-team__heartbeat">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 85.31 76.61">
+                <g id="a">
+                    <path d="M71.36,42.01v3.12h-9.88l-2.17-6.39c-.29-.85-.96-.9-1.13-.9-.2,0-.84,.06-1.14,.89l-.06,.16-3.81,8.28h-3.78l-2.22,20.91-2.59-.11c-.15-3.2-.33-6.71-.51-10.38,0-.01-.01-.01-.01-.02-.02-.36-.04-.73-.05-1.09-.1-2-.21-4.04-.32-6.1-.53-9.73-1.12-19.72-1.65-27.01l-3.96,34.03-1.8-5.6c-.66-2.05-1.66-4.21-2.15-4.68h-.38c-2.74-.14-3.63-.27-4.15-.57-1.49-.86-2.34-2.55-3.1-4.05-.41-.83-1.04-2.08-1.39-2.09-.64-.04-.92,.34-1.7,1.74-.3,.54-.64,1.16-1.06,1.71-1,1.31-2.71,2.64-4.28,2.64h-4.12v-3.13h4.12c.61,0,1.69-.75,2.37-1.63,.28-.37,.54-.83,.81-1.33,.75-1.35,1.77-3.23,3.94-3.13,1.76,.08,2.7,1.94,3.54,3.6,.59,1.18,1.2,2.4,1.99,2.84,.36,.13,2.37,.22,3.13,.27l.55,.03c.8,.04,1.59,.73,2.38,2.06l4.37-37.56,1.84,2.11c.38,.46,1.22,1.41,3.07,35.13h0v.02c.09,1.67,.18,3.42,.28,5.26v.16l.03-.23,.54-5.02c.1-1.08,.87-1.9,1.79-1.9h2.94l3.04-6.61c.62-1.69,1.95-2.73,3.49-2.73h.02c1.57,0,2.92,1.09,3.52,2.86l1.51,4.44h8.14Z"
+                    />
+                </g>
+                <g id="b"/>
+                <g id="c"/>
+            </svg>
+        </div>
+        <h2 class="meet-the-team__heading">
+            <?php echo $meet_heading; ?>
+        </h2>
+    <?php endif; ?>
+
+    <?php if ( get_field( 'meet_the_team_readmore' ) ) : ?>
+        <?php $readMore = get_field( 'meet_the_team_readmore' ); ?>
+    <?php endif; ?>
+
+    <?php if ( $teamMembers->have_posts() ) : ?>
+        <div class="meet-the-team__members">
+            <?php while ( $teamMembers->have_posts() ) : $teamMembers->the_post(); ?>
+                <div class="members__member">
+                    <div class="member__image-block">
+                        <?php the_post_thumbnail(); ?>
+                    </div>
+                    <div class="member__info-block">
+                        <h2 class="member__heading">
+                            <?php the_title(); ?>
+                        </h2>
+                        <p class="member__text">
+                            <?php the_excerpt(); ?>
+                        </p>
+                        <?php if ( isset( $readMore ) ) : ?>
+                            <a href="<?php echo $readMore['url'] ?>" target="<?php echo $readMore['target'] ?>"
+                               title="<?php echo $readMore['title'] ?>" class="member__readmore">
+                                <?php echo $readMore['title']; ?>
+                            </a>
+                        <?php endif; ?>
+                    </div>
+                </div>
+            <?php endwhile; ?>
+            <?php wp_reset_postdata(); ?>
+        </div>
+    <?php endif; ?>
 </div>
 
 <?php if ( have_posts() ) : while ( have_posts() ) :
