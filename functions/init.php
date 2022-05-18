c210717382d47477c7691cf33da766b6975efbab
FaabsD <fabian.et.je@hotmail.com>
2022-05-18 17:05:09 +0200

    fixed my custom_pagination function

--- functions/init.php
+++ functions/init.php
@@ -217,49 +217,36 @@ function custom_excerpt_length( $size )
  * @param int $pages
  * @param int $range
  */
-function custom_pagination( $pages = 0, $range = 2 )
+function custom_pagination( $pages = '', $range = 2 )
 {
-
-    $showItems = ( $range * 2 ) + 1;
+    $showitems = ( $range * 2 ) + 1;
 
     global $paged;
-
     if ( empty( $paged ) ) $paged = 1;
 
-    if ( $pages == 0 ) {
-
+    if ( $pages == '' ) {
         global $wp_query;
-
         $pages = $wp_query->max_num_pages;
-
         if ( !$pages ) {
             $pages = 1;
         }
-
     }
-
-    $result = '';
 
     if ( 1 != $pages ) {
-
-        $result = '<ul class="pagination">';
+        echo "<div class='pagination'>";
+        if ( $paged > 2 && $paged > $range + 1 && $showitems < $pages ) echo "<a href='" . get_pagenum_link( 1 ) . "'>&laquo;</a>";
+        if ( $paged > 1 && $showitems < $pages ) echo "<a href='" . get_pagenum_link( $paged - 1 ) . "'>&lsaquo;</a>";
 
         for ( $i = 1; $i <= $pages; $i++ ) {
-
-            if ( 1 != $pages && ( !( $i >= $paged + $range + 1 || $i <= $paged - $range - 1 ) || $pages <= $showItems ) ) {
-
-                $result .= ( $paged == $i ) ? '<li class="active"><a href="' . get_pagenum_link( $i ) . '"><span>' . $i . '</span></a></li>' : '<li><a href=">' . get_pagenum_link( $i ) . '" class="g-pagination-item"><span>' . $i . '</span></a></li>';
-
+            if ( 1 != $pages && ( !( $i >= $paged + $range + 1 || $i <= $paged - $range - 1 ) || $pages <= $showitems ) ) {
+                echo ( $paged == $i ) ? "<span class='current'>" . $i . "</span>" : "<a href='" . get_pagenum_link( $i ) . "' class='inactive' >" . $i . "</a>";
             }
-
         }
 
-        $result .= '</ul>';
-
+        if ( $paged < $pages && $showitems < $pages ) echo "<a href='" . get_pagenum_link( $paged + 1 ) . "'>&rsaquo;</a>";
+        if ( $paged < $pages - 1 && $paged + $range - 1 < $pages && $showitems < $pages ) echo "<a href='" . get_pagenum_link( $pages ) . "'>&raquo;</a>";
+        echo "</div>\n";
     }
-
-    return $result;
-
 }
 
 /**
