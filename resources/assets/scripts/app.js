e37894d6c9b1bc3bb0b0783d6ae647a50b250b23
FaabsD <fabian.et.je@hotmail.com>
2022-05-18 15:34:32 +0200

    require submit-form.js

--- resources/assets/scripts/app.js
+++ resources/assets/scripts/app.js
@@ -2,6 +2,7 @@ $ = jQuery;
 
 require('./modules/3d-preview');
 require('./modules/navigationbar');
+require('./modules/submit-form');
 require( './modules/tabswitch');
 require('./modules/team-slider');
 
