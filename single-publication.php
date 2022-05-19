ca659f01d4ff8ef13dca9e834d685d3adfe52ee0
FaabsD <fabian.et.je@hotmail.com>
2022-05-19 22:20:18 +0200

    added a single page template for publications posttype

---
+++ single-publication.php
@@ -0,0 +1,32 @@
+<?php get_header(); ?>
+
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    <main class="publication">
+        <header>
+            <?php if ( get_field( 'publication_date' ) ) : ?>
+                <h3>
+                    Published:&nbsp;
+                    <?php the_field( 'publication_date' ); ?>
+                </h3>
+            <?php endif; ?>
+            <h1>
+                <?php the_title(); ?>
+            </h1>
+            <h2 class="authors">
+                <span class="authors__head">Authors:&nbsp;</span>
+                <?php if ( get_field( 'authors' ) ) {
+                    the_field( 'authors' );
+                } ?>
+            </h2>
+            <a class="scholar-link" href="<?php the_field('google_scholar_url'); ?>">
+                Google Scholar
+            </a>
+        </header>
+        <article>
+            <?php the_content(); ?>
+        </article>
+    </main>
+
+<?php endwhile; endif; ?>
+
+<?php get_footer(); ?>
