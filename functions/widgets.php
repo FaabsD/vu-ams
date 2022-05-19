a06c4aa9ff3363b767fc769b3b4568ac5fe2811a
FaabsD <fabian.et.je@hotmail.com>
2022-05-19 21:26:13 +0200

    registered widget areas for the footer

--- functions/widgets.php
+++ functions/widgets.php
@@ -3,7 +3,7 @@
  * Register theme widgets
  */
 
-add_action('init', 'register_widgets');
+add_action( 'init', 'register_widgets' );
 
 function register_widgets()
 {
@@ -11,6 +11,33 @@ function register_widgets()
         'name'          => 'navbar call to action',
         'id'            => 'navbar_menu_section',
         'before_widget' => '<div class="navigation__cta">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h2>',
+        'after_title'   => '</h2>',
+    ) );
+
+    register_sidebar( array(
+        'name'          => 'Footer Address',
+        'id'            => 'footer_address',
+        'before_widget' => '<div class="footer__address">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h2>',
+        'after_title'   => '</h2>',
+    ) );
+
+    register_sidebar( array(
+        'name'          => 'Footer Sitemap',
+        'id'            => 'footer_sitemap',
+        'before_widget' => '<div class="footer__sitemap">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h2>',
+        'after_title'   => '</h2>',
+    ) );
+
+    register_sidebar( array(
+        'name'          => 'Footer Call to Action',
+        'id'            => 'footer_call_to_action',
+        'before_widget' => '<div class="footer__cta">',
         'after_widget'  => '</div>',
         'before_title'  => '<h2>',
         'after_title'   => '</h2>',
