91d088824457f4616ef68b2974b2e067b265cfe5
FaabsD <fabian.et.je@hotmail.com>
2022-05-20 15:38:45 +0200

    added a measurement section

--- page-hardware.php
+++ page-hardware.php
@@ -4,7 +4,9 @@
 <?php get_header(); ?>
 
 <header>
-    <?php the_title(); ?>
+    <h1>
+        <?php the_title(); ?>
+    </h1>
     <?php if ( have_rows( 'specifications' ) ) : while ( have_rows( 'specifications' ) ) : the_row(); ?>
         <section>
             <?php if ( get_sub_field( 'head' ) ) : ?>
@@ -18,14 +20,25 @@
 
         </section>
         <section>
-            <?php if (get_sub_field('hardware_image')) : ?>
-            <?php $hardwareImg = get_sub_field('hardware_image'); ?>
-                <img src="<?php echo $hardwareImg['url']?>" alt="<?php echo $hardwareImg['alt']?>">
+            <?php if ( get_sub_field( 'hardware_image' ) ) : ?>
+                <?php $hardwareImg = get_sub_field( 'hardware_image' ); ?>
+                <img src="<?php echo $hardwareImg['url'] ?>" alt="<?php echo $hardwareImg['alt'] ?>">
             <?php endif; ?>
         </section>
     <?php endwhile; endif; ?>
 </header>
-<main>
-
+<main class="content">
+    <section class="content__measurement">
+        <?php if ( have_rows( 'measurement' ) ) : while ( have_rows( 'measurement' ) ) : the_row(); ?>
+            <?php if ( get_sub_field( 'head' ) ) : ?>
+                <h3>
+                    <?php the_sub_field( 'head' ); ?>
+                </h3>
+            <?php endif; ?>
+            <?php if ( get_sub_field( 'content' ) ) : ?>
+                <?php the_sub_field( 'content' ); ?>
+            <?php endif; ?>
+        <?php endwhile; endif; ?>
+    </section>
 </main>
 <?php get_footer(); ?>
