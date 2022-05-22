67fadf9560199207b7b2b8d9415dbf8e5870cb07
FaabsD <fabian.et.je@hotmail.com>
2022-05-22 15:14:29 +0200

    created base for software page template

---
+++ page-software.php
@@ -0,0 +1,16 @@
+<?php
+// Template name: Software
+?>
+<?php get_header(); ?>
+
+<header>
+    
+</header>
+
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<div class="content">
+
+</div>
+<?php endwhile; endif; ?>
+
+<?php get_footer(); ?>
