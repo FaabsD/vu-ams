910ae21dd8a33d1eeabfa0676dec94fe1b0417bf
FaabsD <fabian.et.je@hotmail.com>
2022-05-22 16:17:49 +0200

    added the header + content from acf fields

--- page-software.php
+++ page-software.php
@@ -3,14 +3,23 @@
 ?>
 <?php get_header(); ?>
 
-<header>
-    
-</header>
-
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<div class="content">
 
-</div>
+    <header>
+        <h1><?php the_title(); ?></h1>
+        <div class="content">
+            <?php if ( have_rows( 'header' ) ) : while ( have_rows( 'header' ) ): the_row(); ?>
+                <?php if ( get_sub_field( 'header_text' ) ) : ?>
+                    <?php the_sub_field( 'header_text' ); ?>
+                <?php endif; ?>
+            <?php endwhile; endif; ?>
+        </div>
+    </header>
+
+
+    <main class="content">
+        <?php the_content(); ?>
+    </main>
 <?php endwhile; endif; ?>
 
 <?php get_footer(); ?>
