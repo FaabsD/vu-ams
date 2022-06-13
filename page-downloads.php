57979de9f5c761fd058000bcdeaca0342dc7c557
FaabsD <fabian.et.je@hotmail.com>
2022-06-13 14:26:52 +0200

    created page-downloads.php

---
+++ page-downloads.php
@@ -0,0 +1,13 @@
+<?php
+// Template Name: Downloads
+?>
+<?php get_header(); ?>
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    <header>
+        <h1><?php the_title();?></h1>
+    </header>
+<main class="content">
+    <?php the_content(); ?>
+</main>
+<?php endwhile; endif; ?>
+<?php get_footer(); ?>
