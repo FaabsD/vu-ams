e4ed5f94f753d6da689d95a19361310eb01ce734
FaabsD <fabian.et.je@hotmail.com>
2022-06-21 14:03:00 +0200

    created a tutorial page template

---
+++ page-tutorial.php
@@ -0,0 +1,15 @@
+<?php
+// Template name: Tutorial page
+?>
+<?php get_header(); ?>
+<div class="content">
+    <?php if (have_posts()): while (have_posts()) : the_post(); ?>
+        <main>
+            <h1>
+                <?php the_title(); ?>
+            </h1>
+            <?php the_content(); ?>
+        </main>
+    <?php endwhile; endif; ?>
+</div>
+<?php get_footer(); ?>
