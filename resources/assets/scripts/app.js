c7296575c6454ce285fccafc00d7ff4e713540d8
FaabsD <fabian.et.je@hotmail.com>
2022-06-28 12:07:13 +0200

    created client-side script to set the height of the thumbnails of the shop page to equal the width

--- resources/assets/scripts/app.js
+++ resources/assets/scripts/app.js
@@ -4,7 +4,8 @@ require('./modules/3d-preview');
 require('./modules/home-header-slider');
 require('./modules/navigationbar');
 require('./modules/previous-downloads');
+require('./modules/set-image-height-client');
 require('./modules/submit-form');
-require( './modules/tabswitch');
+require('./modules/tabswitch');
 require('./modules/team-slider');
 
