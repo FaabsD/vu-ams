12bbb0141628a8dd96f82305cb12172c484d8a0e
FaabsD <fabian.et.je@hotmail.com>
2022-07-04 11:52:56 +0200

    created a shooping cart page template

---
+++ page-cart.php
@@ -0,0 +1,17 @@
+<?php
+// Template name: Shopping cart
+?>
+<?php get_header(); ?>
+<div class="content">
+    <?php if (have_posts()): while (have_posts()) : the_post(); ?>
+    <header>
+        <h1>
+            <?php the_title(); ?>
+        </h1>
+    </header>
+        <main>
+            <?php the_content(); ?>
+        </main>
+    <?php endwhile; endif; ?>
+</div>
+<?php get_footer(); ?>
