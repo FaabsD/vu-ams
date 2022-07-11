d697309c1d43c4a2310980d453b4b63ee8037f01
FaabsD <fabian.et.je@hotmail.com>
2022-07-11 14:44:08 +0200

    created page-my_account teemplate file with corresponding stylesheet with a styling start

---
+++ page-my_account.php
@@ -0,0 +1,22 @@
+<?php
+// Template name: My account
+?>
+
+<?php get_header(); ?>
+
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+<div class="content">
+	<header>
+		<h1>
+			<?php the_title(); ?>
+		</h1>
+	</header>
+	<main>
+		<?php the_content();?>
+	</main>
+</div>
+
+<?php endwhile;
+endif; ?>
+
+<?php get_footer(); ?>
