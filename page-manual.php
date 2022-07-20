33e545c85549a9543a07ec9059deb894ddfb5fa7
FaabsD <fabian.et.je@hotmail.com>
2022-07-20 12:20:18 +0200

    made a start to a manual page template with styling

---
+++ page-manual.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Template Name: Manual Page
+ *
+ * @package WordPress
+ * @subpackage VU-AMS
+ * */
+
+?>
+<?php get_header(); ?>
+
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+<header>
+	<h1>
+		<?php the_title(); ?>
+	</h1>
+</header>
+
+<main class="content">
+	<?php the_content(); ?>
+</main>
+
+<?php endwhile; endif; ?>
+
+<?php get_footer(); ?>
+
