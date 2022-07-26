61468bf957949c0e6cbed3efd0e6d0034b6338fa
FaabsD <fabian.et.je@hotmail.com>
2022-07-26 14:59:10 +0200

    made a change to the header structure so an image can be added through extra fields

--- page-software.php
+++ page-software.php
@@ -3,15 +3,23 @@
 ?>
 <?php get_header(); ?>
 
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
     <header>
         <h1><?php the_title(); ?></h1>
         <div class="content">
-            <?php if ( have_rows( 'header' ) ) : while ( have_rows( 'header' ) ): the_row(); ?>
-                <?php if ( get_sub_field( 'header_text' ) ) : ?>
-                    <?php the_sub_field( 'header_text' ); ?>
+            <?php if (have_rows('header')) : while (have_rows('header')): the_row(); ?>
+                <?php if (get_sub_field('header_text')) : ?>
+					<div class="content__text">
+						<?php the_sub_field('header_text'); ?>
+					</div>
                 <?php endif; ?>
+				<?php if (get_sub_field('header_image')) : ?>
+					<?php $header_image = get_sub_field('header_image'); ?>
+					<figure class="content__image">
+						<img src="<?php echo $header_image['url']?>" alt="<?php echo $header_image['alt'] ?>" >
+					</figure>
+				<?php endif;?>
             <?php endwhile; endif; ?>
         </div>
     </header>
