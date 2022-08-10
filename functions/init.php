b76179bf5fc69d1cfce694ce2559cce955c74bd1
FaabsD <fabian.et.je@hotmail.com>
2022-08-10 14:21:25 +0200

    created a function to determine if the full content wordcount of a post is greater then the excerpt wordcount and if the excerpt wordcount is greater then 1

--- functions/init.php
+++ functions/init.php
@@ -373,3 +373,28 @@ function getPublicationDates()
     return false;
     endif;
 }
+
+function check_longer_than_excerpt($excerpt, $full_text = null) {
+	// get excerpt length
+	$excerptWordCount = (str_word_count($excerpt) > 1) ? str_word_count($excerpt) -1 : str_word_count($excerpt);
+	// $full_text_word_count = explode(' ', strip_tags($full_text));
+	$full_text_word_count = preg_split('/[\s,.:]+/', htmlentities(strip_tags($full_text)));
+	// htmlentities()
+
+
+	if (defined('WP_DEBUG')) {
+		error_log("======== DEBUGGING THE CHECK_LONGER_THAN_EXCERPT FUNCTION ========");
+		error_log("Full text wordcount = " . count($full_text_word_count));
+		error_log("excerp wordcount = " . $excerptWordCount);
+
+		error_log("fulltext wordcount higher then or equal to excerpt wordcount =  ". (count($full_text_word_count) >= $excerptWordCount) ? "true" : "false");
+		error_log('====== Original text stripped from tags ======');
+		error_log(htmlentities(strip_tags($full_text)));
+	}
+
+	if(is_array($full_text_word_count) && count($full_text_word_count) >= $excerptWordCount && $excerptWordCount > 1) {
+		return true;
+	}
+
+	return false;
+}
