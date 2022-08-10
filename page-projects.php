85cca7f6b0e3a647f219c56cc6fcf6942df6359c
FaabsD <fabian.et.je@hotmail.com>
2022-08-10 15:04:05 +0200

    finished projects page structure and added styling

--- page-projects.php
+++ page-projects.php
@@ -32,6 +32,30 @@
 
 		$projectsQuery = new WP_Query($projectsArgs);
 	?>
+
+	<?php if ($projectsQuery->have_posts()) : while($projectsQuery->have_posts()) : $projectsQuery->the_post(); ?>
+
+		<div class="project">
+			<h3 class="project__title">
+				<?php the_title(); ?>
+			</h3>
+			<div class="project__content">
+				<?php the_excerpt(); ?>
+			</div>
+			<?php if (check_longer_than_excerpt(get_the_excerpt($post->ID), get_the_content($post->ID))) : ?>
+				<div class="project__read-more">
+					<a href="<?php the_permalink(); ?>" class="read-more__button">
+						<?php _e('Read more', THEME_TEXT_DOMAIN); ?>
+					</a>
+				</div>
+			<?php endif; ?>
+		</div>
+	<?php endwhile; ?>
+	<?php else : ?>
+		<div class="no-results">
+			<?php _e('No projects found', THEME_TEXT_DOMAIN); ?>
+		</div>
+	<?php endif; ?>
 </div>
 
 <?php get_footer(); ?>
