909b9d5d66af361fa2c6825fdc82679d62d27738
FaabsD <fabian.et.je@hotmail.com>
2022-09-05 13:13:08 +0200

    expanded script to make product image/thumbnail equal wiidth/height to work on products on other pages using handpicked products block

--- resources/assets/scripts/modules/set-image-height-client.js
+++ resources/assets/scripts/modules/set-image-height-client.js
@@ -1,6 +1,8 @@
 const shopThumbs = document.querySelectorAll('.products .product .woocommerce-loop-product__link .attachment-woocommerce_thumbnail');
 const shopPlaceholders = document.querySelectorAll('.products .product .woocommerce-loop-product__link .woocommerce-placeholder');
 
+const handPickedProductsContainer = $('.wp-block-handpicked-products , .wc-block-handpicked-products');
+
 $(document).ready(function () {
 
     if (shopThumbs) {
@@ -28,6 +30,29 @@ $(document).ready(function () {
                 equalize_image_height_to_width(placeholder);
             });
         });
+    }
+
+    if (handPickedProductsContainer) {
+        console.log("SHOW HAND PICKED PRODUCTS INFO");
+        console.log(handPickedProductsContainer);
+        console.log("SHOW HANDPICKED PRODUCTS CONTAINER CHILDREN");
+
+        const handPickedProductsInner = handPickedProductsContainer.children(0);
+        console.log(handPickedProductsInner);
+
+        console.log('LOOP THROUGH EVERY PRODUCT IN THE INNER CONTAINER');
+        handPickedProductsInner.children().each(function (index) {
+            console.log('FIND THE THUMBNAIL OR PLACEHOLDER OF THE PRODUCT');
+            let productThumb = $(this).find('.wc-block-grid__product-link .wc-block-grid__product-image .attachment-woocommerce_thumbnail , .wc-block-grid__product-link .wc-block-grid__product-image .woocommerce-placeholder');
+
+            if (productThumb.length >= 1) {
+                // equalize_image_height_to_width(productThumb);
+                console.log("PRODUCT THUMBNAIL DATA");
+                console.log(productThumb);
+
+                equalize_image_height_to_width(productThumb.get(0));
+            }
+        })
     }
 
 });
