6de31b526e5ce550b8faac0d1b02c9b2f481b002
FaabsD <fabian.et.je@hotmail.com>
2022-10-05 14:52:42 +0200

    update functions.php

--- functions.php
+++ functions.php
@@ -2,6 +2,8 @@
 
 require_once 'vendor/autoload.php';
 require_once 'classes/Scholar.php';
+require_once 'classes/Zotero.php';
+
 require_once 'functions/init.php';
 
 require_once 'functions/menus.php';
@@ -17,3 +19,6 @@ require_once 'functions/taxonomies.php';
 require_once 'functions/widgets.php';
 
 require_once 'functions/update-publication.php';
+
+require_once 'functions/publications.php';
+
