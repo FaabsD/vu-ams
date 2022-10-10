509428ce5241c9510d20c4e43806cfbfa6c4bf44
FaabsD <fabian.et.je@hotmail.com>
2022-10-10 15:59:48 +0200

    make additional api calls when there is more total result then result from last call

--- functions/publications.php
+++ functions/publications.php
@@ -10,12 +10,14 @@ function get_publications_from_zotero() {
     $groupID    = ( defined( 'ZOTERO_GROUP_ID' ) && null !== ZOTERO_GROUP_ID ) ? ZOTERO_GROUP_ID : 0;
     $collection = ( defined( 'ZOTERO_COLLECTION_ID' ) && null !== ZOTERO_COLLECTION_ID ) ? ZOTERO_COLLECTION_ID : null;
 
+    $resultsLimit = (defined('ZOTERO_RESULT_LIMIT')) ? ZOTERO_RESULT_LIMIT : 100;
+
     error_log( '===== SETUP ZOTERO API CALL USER_ID, GROUP_ID, COLLECTION ========' );
     error_log( 'user id: ' . $userID );
     error_log( 'group id: ' . $groupID );
     error_log( 'collection: ' . $collection );
 
-    $zotero = new Zotero( $userID, $groupID, $collection, 'json' );
+    $zotero = new Zotero( $userID, $groupID, $collection, 'json', array('limit' => $resultsLimit));
 
     // put the API response in a variable
     $response = $zotero->makeAPICall();
@@ -24,14 +26,20 @@ function get_publications_from_zotero() {
         error_log('======== DATA FROM API CALL ========');
         error_log('response type: ' . gettype($response));
     }
-    $responseJSON = json_decode($response);
+    $responseBody = json_decode($response['body']);
 
     if(defined('WP_DEBUG')) {
-        error_log('formatted response data type: ' . gettype($responseJSON));
+        error_log('formatted response data type: ' . gettype($responseBody));
+        error_log('===== response body =====');
+        error_log(print_r($responseBody, true));
+        error_log('======== GET RESPONSE HEADER INFORMATION ========');
+        error_log('type: ' . gettype($response['header']));
+        error_log('contents: ');
+        error_log(print_r($response['headers'], true));
     }
 
-    if (gettype($responseJSON) === 'array') {
-        foreach ($responseJSON as $item) {
+    if (gettype($responseBody) === 'array') {
+        foreach ($responseBody as $item) {
 
             // put the data that will be saved in variables
             $version = $item->data->version;
@@ -124,8 +132,163 @@ function get_publications_from_zotero() {
                  if(get_field('version', $existing_post) && get_field('version', $existing_post) !== $version) {
                     if (defined('WP_DEBUG')) {
                         error_log('update existing publication');
+
+                        save_publication( $title, array( 'post_content' => $abstract, 'ID' => $existing_post ), $pubMetaValues );
+
                     }
                  }
+            }
+        }
+
+        // make consecutive calls 
+        $callCount = 1;
+        $makeConsecutiveCalls = false;
+
+        $totalResultsCount = (int) $response['headers']['Total-Results'];
+
+        if(defined('WP_DEBUG')) {
+            error_log('======== GET SET DATA FOR CONSECUTIVE API CALLS ========');
+            error_log('Call count: ' . $callCount);
+            error_log( 'Results Limit: ' . $resultsLimit );
+            error_log('Total Results: ' . $totalResultsCount);
+            error_log("Total Results datatype: " . gettype($totalResultsCount));
+            error_log('First results count: ' . count($responseBody));
+            error_log('First results Count Type: ' . gettype(count($responseBody)));
+
+        }
+
+        if(isset($totalResultsCount) && $totalResultsCount > count($responseBody) ) {
+            if(defined('WP_DEBUG')) {
+                error_log('THE TOTAL NUMBER OF PUBLICATION RESULTS IS GREATER THEN THE NUMBER RETRIEVED MAKE FOLLOW-UP CALLS');
+            }
+
+            $makeConsecutiveCalls = true;
+
+            while($makeConsecutiveCalls) {
+                // make a new call with start
+                $start = $callCount*$resultsLimit;
+                // create a new Zotero class instance
+                $api = new Zotero($userID, $groupID, $collection, 'json', array('limit' => $resultsLimit, 'start' => $start));
+                $apiResponse = $api->makeAPICall();
+
+                $apiResponseBody = json_decode($apiResponse['body']);
+
+                if (gettype($apiResponseBody) === 'array') {
+                    foreach ($apiResponseBody as $item) {
+
+                        // put the data that will be saved in variables
+                        $version = $item->data->version;
+                        $itemType = $item->data->itemType;
+                        $title = $item->data->title;
+                        $creators = $item->data->creators;
+                        $abstract = $item->data->abstractNote;
+                        $publicationTitle = $item->data->publicationTitle;
+                        $volume = $item->data->volume;
+                        $pages = $item->data->pages;
+                        $date = $item->data->date;
+                        $series = $item->data->series;
+                        $seriesTitle = $item->data->seriesTitle;
+                        $seriesText = $item->data->seriesText;
+                        $journalAbbreviation = $item->data->journalAbbreviation;
+                        $DOI = "https://www.doi.org/" . $item->data->DOI;
+                        $ISSN = $item->data->ISSN;
+                        $shortTitle = $item->data->shortTitle;
+                        // URL only as a fallback option if there is no Doi
+                        $url = $item->data->url;
+                        $libraryCatalog = $item->data->libraryCatalog;
+                        $tags = $item->data->tags;
+            
+            
+                        if(defined('WP_DEBUG')) {
+                            error_log("====== ITEM DATA ======");
+                            error_log('version: ' . $version);
+                            error_log("itemType: " . $itemType);
+                            error_log('title: ' . $title);
+                            error_log('creators:');
+                            foreach ($creators as $author) {
+                                    error_log( $author->firstName . ' ' . $author->lastName );
+            
+                            }
+                            error_log('abstractNote: ' . $abstract);
+                            error_log("publicationTitle: " . $publicationTitle);
+                            error_log('volume: ' . $volume);
+                            error_log('pages: ' . $pages);
+                            error_log('date: ' . $date);
+                            error_log('series: ' . $series);
+                            error_log('seriesTitle: ' . $seriesTitle);
+                            error_log('seriesText: ' . $seriesText);
+                            error_log('journalAbbreviation: ' . $journalAbbreviation);
+                            error_log('DOI: ' . $DOI);
+                            error_log('ISSN: ' . $ISSN);
+                            error_log('shortTitle: ' . $shortTitle);
+                            error_log('url: ' . $url);
+                            error_log('libraryCatalog: ' . $libraryCatalog);
+                            error_log('tags:');
+                            foreach ($tags as $tag) {
+                                error_log($tag->tag);
+                            }
+                        }
+            
+            
+                        // save the values for the custom fields in an array
+            
+                        $pubMetaValues = array(
+                            'version'              => $version,
+                            'item_type'            => $itemType,
+                            'authors'              => $creators,
+                            'publication_title'    => $publicationTitle,
+                            'volume'               => $volume,
+                            'pages'                => $pages,
+                            'publication_date'     => $date,
+                            'series'               => $series,
+                            'series_title'         => $seriesTitle,
+                            'series_text'          => $seriesText,
+                            'journal_abbreviation' => $journalAbbreviation,
+                            'doi'                  => $DOI,
+                            'issn'                 => $ISSN,
+                            'short_title'          => $shortTitle,
+                            'url'                  => $url,
+                            'library_catalog'      => $libraryCatalog,
+                            'tags'                 => $tags,
+                        );
+            
+                        // check if post exists by title
+            
+                        if (post_exists($title, '', '', 'publication') === 0) {
+                            // go save the publication
+                            save_publication( $title, array( 'post_content' => $abstract ), $pubMetaValues );
+            
+                        } else {
+                            // a post with the same title was found
+                            // get the existing posts id
+                            $existing_post = post_exists($title, '', '', 'publication');
+                             // update post only if version number is present and different from current version number
+            
+                             if(get_field('version', $existing_post) && get_field('version', $existing_post) !== $version) {
+                                if (defined('WP_DEBUG')) {
+                                    error_log('update existing publication');
+            
+                                    save_publication( $title, array( 'post_content' => $abstract, 'ID' => $existing_post ), $pubMetaValues );
+            
+                                }
+                             }
+                        }
+                    }
+                }
+
+                $callCount++;
+
+                if(defined('WP_DEBUG')) {
+                    error_log('Total API call count: ' . $callCount);
+                }
+
+                if(!($callCount*$resultsLimit < $totalResultsCount)) {
+                    if(defined('WP_DEBUG')) {
+                        error_log('STOP MAKING CONSECUTIVE API CALLS');
+                        $makeConsecutiveCalls = false;
+                    }
+                }
+
             }
         }
     }
