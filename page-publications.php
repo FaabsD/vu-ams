2e5447182b172dd248b98cbe7a3fac6b8444fcc0
FaabsD <fabian.et.je@hotmail.com>
2022-10-12 09:36:28 +0200

    reformatted code using php-cs-fixer

--- page-publications.php
+++ page-publications.php
@@ -14,42 +14,44 @@
 <?php endwhile; endif; ?>
 
 <?php
-$meta_query = array();
-if ( isset( $_GET['author'] ) && !empty( $_GET['author'] ) ) {
-    $meta_query[] = array(
-        'key'     => 'authors',
-        'value'   => $_GET['author'],
-        'compare' => "LIKE",
-    );
-}
-if ( isset( $_GET['date'] ) && $_GET['date'] != 'Date' ) {
-    $meta_query[] = array(
-        'key'     => 'publication_date',
-        'value'   => $_GET['date'],
-        'compare' => '=',
-    );
-}
+    $meta_query = array();
 
-if ( isset( $_GET['tags'] ) && !empty( $_GET['tags'] ) ) {
-    $meta_query[] = array(
-        'key'     => 'tags',
-        'value'   => $_GET['tags'],
-        'compare' => 'LIKE',
-    );
-}
+    if ( isset( $_GET['author'] ) && !empty( $_GET['author'] ) ) {
+        $meta_query[] = array(
+            'key'     => 'authors',
+            'value'   => $_GET['author'],
+            'compare' => 'LIKE',
+        );
+    }
 
-$paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
-$args = array(
-	's'              => isset( $_GET['search'] ) && $_GET['search'] != '' ? $_GET['search'] : '',
-	'post_type'      => 'publication',
-	'meta_query'     => $meta_query,
-	'posts_per_page' => 10,
-	'paged'          => $paged,
-	'meta_key' 		 => 'publication_date',
-	'orderby'	     => 'meta_value_num',
-	'order'			 => 'DESC',
-);
-$query = new WP_Query( $args );
+    if ( isset( $_GET['date'] ) && $_GET['date'] != 'Date' ) {
+        $meta_query[] = array(
+            'key'     => 'publication_date',
+            'value'   => $_GET['date'],
+            'compare' => '=',
+        );
+    }
+
+    if ( isset( $_GET['tags'] ) && !empty( $_GET['tags'] ) ) {
+        $meta_query[] = array(
+            'key'     => 'tags',
+            'value'   => $_GET['tags'],
+            'compare' => 'LIKE',
+        );
+    }
+
+    $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
+    $args  = array(
+        's'              => isset( $_GET['search'] ) && $_GET['search'] != '' ? $_GET['search'] : '',
+        'post_type'      => 'publication',
+        'meta_query'     => $meta_query,
+        'posts_per_page' => 10,
+        'paged'          => $paged,
+        'meta_key' 		 => 'publication_date',
+        'orderby'	     => 'meta_value_num',
+        'order'			 => 'DESC',
+    );
+    $query = new WP_Query( $args );
 ?>
 <form action="<?php the_permalink(); ?>" method="get" id="pubForm">
     <table class="pub-table">
@@ -57,18 +59,18 @@ $query = new WP_Query( $args );
         <tr>
             <td id="searchCell">
                 <input type="text" name="search" id="search"
-                       value="<?php echo isset( $_GET['search'] ) && !empty( $_GET['search'] ) ? $_GET['search'] : '' ?>"
+                       value="<?php echo isset( $_GET['search'] ) && !empty( $_GET['search'] ) ? $_GET['search'] : ''; ?>"
                        placeholder="<?php _e( 'Search', THEME_TEXT_DOMAIN ); ?>">
             </td>
             <td>
                 <input type="text" name="author" id="author"
-                       value="<?php echo isset( $_GET['author'] ) && !empty( $_GET['author'] ) ? $_GET['author'] : '' ?>"
+                       value="<?php echo isset( $_GET['author'] ) && !empty( $_GET['author'] ) ? $_GET['author'] : ''; ?>"
                        placeholder="<?php _e( 'Search', THEME_TEXT_DOMAIN ); ?>">
             </td>
             <td>
                 <?php
-                $dateArr = array();
-                $pubDates = getPublicationDates();
+                    $dateArr = array();
+                    $pubDates = getPublicationDates();
                 ?>
                 <?php if ( $pubDates ) {
                     foreach ( $pubDates as $date ) {
@@ -82,8 +84,8 @@ $query = new WP_Query( $args );
                 <select name="date" id="date">
                     <option value="Date">Date</option>
                     <?php foreach ( $dateArr as $date ) : ?>
-                        <option <?php echo isset( $_GET['date'] ) && $_GET['date'] === $date ? 'selected' : '' ?>
-                                value="<?php echo $date ?>">
+                        <option <?php echo isset( $_GET['date'] ) && $_GET['date'] === $date ? 'selected' : ''; ?>
+                                value="<?php echo $date; ?>">
                             <?php echo $date; ?>
                         </option>
                     <?php endforeach; ?>
@@ -116,9 +118,9 @@ $query = new WP_Query( $args );
                     </td>
                     <td>
                         <?php
-                        if ( get_field( 'publication_date' ) ) {
-                            the_field( 'publication_date' );
-                        };
+                            if ( get_field( 'publication_date' ) ) {
+                                the_field( 'publication_date' );
+                            }
                         ?>
                     </td>
                 </tr>
@@ -139,7 +141,7 @@ $query = new WP_Query( $args );
         <tr>
             <td></td>
             <td colspan="2" class="pagination-cell">
-                <?php custom_pagination( $query->max_num_pages ) ?>
+                <?php custom_pagination( $query->max_num_pages ); ?>
             </td>
         </tr>
         </tfoot>
