39cff2a29568b93173de6cc02332f80ce9dd988e
FaabsD <fabian.et.je@hotmail.com>
2022-11-16 17:12:04 +0100

    enqueued select2 styling and changed slick styling path

--- functions/enqueue.php
+++ functions/enqueue.php
@@ -4,8 +4,13 @@ add_action('wp_enqueue_scripts', 'enqueue_styles');
 function enqueue_styles()
 {
     wp_enqueue_style('VU-AMS_style', THEME_URL . '/build/css/app.css');
-    wp_enqueue_style('slickcss', THEME_URL. '/resources/slick/slick.css');
+
+    wp_enqueue_style( 'slickcss', THEME_URL . 'node_modules/slick-carousel/slick/slick.css' );
+
     wp_enqueue_style( 'leaflet', THEME_URL . '/node_modules/leaflet/dist/leaflet.css' );
+
+
+    wp_enqueue_style( 'select2', THEME_URL . '/node_modules/select2/dist/css/select2.css' );
 
 }
 
@@ -13,8 +18,7 @@ add_action('wp_enqueue_scripts', 'enqueue_scripts');
 
 function enqueue_scripts()
 {
-    wp_enqueue_script('app', THEME_URL . '/build/scripts/app.js', array('jquery'), theme_version(), true);
-    wp_enqueue_script('slickslider', THEME_URL . '/resources/slick/slick.js', array('jquery'), theme_version(), true);
+    wp_enqueue_script( 'app', THEME_URL . '/build/scripts/app.js', array( 'jquery' ), theme_version(), true );
 
     //localise THEME_URL to use in script
     $localize = array('theme_url' => THEME_URL);
