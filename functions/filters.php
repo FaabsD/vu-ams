905780ee763ce4195a55a717f8833a224c6db3ca
FaabsD <fabian.et.je@hotmail.com>
2022-11-30 12:03:16 +0100

    created a filter on the tag-group posttype to prefill with tags

--- functions/filters.php
+++ functions/filters.php
@@ -8,4 +8,44 @@ add_action( 'after_setup_theme', 'ams_add_filters' );
  * @return void
  */
 function ams_add_filters() {
+
+    // prefill the tag-group posttype with all the available tags
+    add_filter( 'default_content', 'ams_set_default_tag_group_content', 10, 2 );
+
+    /**
+     * Prefill the content of new posts of the tag-group post type with the most used tags in publications.
+     * Tags will be separated by comma's and spaces
+     *
+     * @param string $content The posts default content
+     * @param object $post    The post to be created
+     *
+     * @return empty|string
+     */
+    function ams_set_default_tag_group_content( $content, $post ) {
+        if ( $post->post_type !== 'tag-group' ) {
+            return $content;
+        }
+        // get all publication tags
+        $unfilteredTags = publicationTags();
+
+        if ( $unfilteredTags && is_array( $unfilteredTags ) ) {
+            /**
+             * filter the tags using the getMostUsedTags helper function
+             * change minimumCount to any desired occurrence count per tag
+             * to preserve it in the resulting array
+             */
+            $filteredTags = getMostUsedTags( $unfilteredTags, 5 );
+        }
+
+        if ( $filteredTags && is_array( $filteredTags ) ) {
+            // turn the filtered tags array into a comma separated string
+            $content = implode( ', ', $filteredTags );
+        } else {
+            // keep content empty
+            $content = '';
+        }
+
+        return $content;
+    }
+
 }
