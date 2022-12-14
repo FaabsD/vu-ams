9f3eaa82df925da0e3e5cf8640f3e1b302c92772
FaabsD <fabian.et.je@hotmail.com>
2022-12-14 17:00:01 +0100

    start getting publication counts by tags

--- resources/assets/scripts/modules/submit-form.js
+++ resources/assets/scripts/modules/submit-form.js
@@ -1,3 +1,43 @@
+function allPostsFromRest(postType, perPage = 10) {
+    const restUrl = `/wp-json/wp/v2/${postType}/?per_page=${perPage}`;
+    let totalPages = 0;
+    let allResults = [];
+
+    // return "trolololololol";
+
+    fetch(restUrl)
+        .then((response) => {
+            if (response.ok) {
+                totalPages = response.headers.get('X-WP-TotalPages');
+                console.log(response);
+                return response.json();
+            }
+            throw new Error(`Failed to fetch posts of type ${postType} from built-in WordPress API`);
+        })
+        .then((json) => {
+            allResults = json;
+
+            for (let i = 2; i <= totalPages; i++) {
+                fetch(restUrl + `&page=${i}`)
+                    .then((response) => {
+                        return response.json();
+                    })
+                    .then((moreResults) => {
+                        allResults.concat(moreResults);
+                    });
+            }
+
+            return allResults;
+        })
+        .catch((error) => {
+            // just in case fetching the locations from the built-in WordPress API fails
+            // open a alert to show the visitor that the locations fetching failed
+
+            alert(`Something went wrong while loading in posts of type ${postType} try refreshing the page or check the console tab from the inspector by right clicking and selecting inspect`);
+            console.error('Something went wrong while fetching locations', error);
+        });
+}
+
 $(document).ready(function (){
     select2 = require('select2');
     const pubForm = document.getElementById('pubForm');
@@ -34,5 +74,13 @@ $(document).ready(function (){
     $('#multiSelect').select2({
         placeholder: "Select or type Tags",
     });
+
+    $('#multiSelect').on('change', function (e) {
+        let selectedTags = $(this).select2('data');
+        console.log(selectedTags);
+
+        console.log('retrieve all publications');
+        console.log(allPostsFromRest('publication', 100));
+    })
 })
 
