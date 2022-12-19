6bec265238e07a4d055fe050e70db9857d31c38b
FaabsD <fabian.et.je@hotmail.com>
2022-12-19 16:52:40 +0100

    Updated to retrieve publications from rest API and to check tags

--- resources/assets/scripts/modules/submit-form.js
+++ resources/assets/scripts/modules/submit-form.js
@@ -1,3 +1,4 @@
+const { AmsWPRest } = require('./ams-classes');
 function allPostsFromRest(postType, perPage = 10) {
     const restUrl = `/wp-json/wp/v2/${postType}/?per_page=${perPage}`;
     let totalPages = 0;
@@ -17,13 +18,23 @@ function allPostsFromRest(postType, perPage = 10) {
         .then((json) => {
             allResults = json;
 
+            console.log(allResults);
+
             for (let i = 2; i <= totalPages; i++) {
+                console.log(totalPages);
+                console.log('===== new rest URL =====');
+                console.log(restUrl + `&page=${i}`);
                 fetch(restUrl + `&page=${i}`)
                     .then((response) => {
+                        console.log(response.ok);
                         return response.json();
                     })
                     .then((moreResults) => {
+                        console.log(`===== EXTRA RESULTS (${i}) =====`);
+                        console.log(moreResults);
                         allResults.concat(moreResults);
+                        console.log('===== NEW results Array =====');
+                        console.log(allResults);
                     });
             }
 
@@ -38,7 +49,7 @@ function allPostsFromRest(postType, perPage = 10) {
         });
 }
 
-$(document).ready(function (){
+$(document).ready(function () {
     select2 = require('select2');
     const pubForm = document.getElementById('pubForm');
     if (pubForm) {
@@ -48,7 +59,7 @@ $(document).ready(function (){
 
         inputFields.forEach((input) => {
             input.addEventListener('keyup', function (e) {
-                if(e.key === "Enter") {
+                if (e.key === "Enter") {
                     pubForm.submit();
                 }
             })
@@ -77,10 +88,66 @@ $(document).ready(function (){
 
     $('#multiSelect').on('change', function (e) {
         let selectedTags = $(this).select2('data');
-        console.log(selectedTags);
+        let selectTags = []
+        selectedTags.forEach(tag => {
+            selectTags.push(tag.text);
+        });
+        console.log(selectTags);
+        let pubsCountBySelectedTags = 0;
 
-        console.log('retrieve all publications');
-        console.log(allPostsFromRest('publication', 100));
+        console.log(selectedTags);
+        // create a new AmsWPRest instance
+        const publicationsRest = new AmsWPRest('publication', 100);
+        let chain = [];
+
+        publicationsRest.makeRestCall().then(postObjects => {
+            // make new API calls
+            for (let i = 1; i <= publicationsRest.totalPagesCount; i++) {
+                console.log(publicationsRest.totalPagesCount);
+                console.log(i)
+                chain.push(publicationsRest.makeRestCall(i));
+                console.log(chain);
+            }
+
+            Promise.allSettled(chain).then(results => {
+                results.forEach(result => {
+                    publicationsRest.setAllResults(result.value);
+
+                    console.log('===== All publication results =====');
+                    console.log(publicationsRest.allPostResults);
+                });
+                // loop through all the retrieved publication objects
+                if (typeof publicationsRest.allPostResults === "object") {
+                    publicationsRest.allPostResults.forEach(publication => {
+                        // console.log(publication.acf);
+                        if (publication.acf.tags !== "") {
+                            // console.log(publication.acf.tags);
+                            // turn tags string into an Array
+                            let pubTagsToArray = publication.acf.tags.split(', ');
+                            // console.log(pubTagsToArray);
+
+                            // check if selected tags are present in the publication object
+                            let tagsChecker = (arr, target) => target.every(v => arr.includes(v));
+
+                            console.log(tagsChecker(selectTags, pubTagsToArray));
+
+                            if (tagsChecker(selectTags, pubTagsToArray)) {
+                                pubsCountBySelectedTags + 1;
+
+                                console.log(`Count of publications containing the selected tags: ${pubsCountBySelectedTags}`);
+                            }
+                        }
+                    })
+                }
+            }).catch(error => {
+                alert('Oops something went wrong fetching all publications from built-in REST API try to reload or check console for more information');
+                console.log(error);
+            })
+        }).catch(error => {
+            alert('Oops something went wrong fetching all publications from built-in REST API try to reload or check console for more information');
+            console.log(error);
+        })
+
     })
 })
 
